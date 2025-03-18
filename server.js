1a0a518e9c0d3bd40cbc174b6be82114d49f9d7d
devanshwebhost <devanshrajput032006@gmail.com>
2025-03-18 08:55:49 +0530

    server.js
    change port ip and cors -- local

--- server.js
+++ server.js
@@ -144,4 +144,6 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Start Server
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
